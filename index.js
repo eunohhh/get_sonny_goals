6e53eade05d4fd33d6e753ad337d1357f36e94af
Eun Oh <bdohhhhh@gmail.com>
2024-07-08 06:24:36 +0900

    240707 [fix] : 전체 로직 수정 puppeteer 사용으로 수정

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 const getGoals = async () => {
     try {
-        const response = await axios.get("https://namu.wiki/w/%EC%86%90%ED%9D%A5%EB%AF%BC", {
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-                Accept: "*/*",
-            },
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://namu.wiki/w/%EC%86%90%ED%9D%A5%EB%AF%BC");
+
+        // 필요한 데이터가 로드될 때까지 기다립니다.
+        await page.waitForSelector("div.OlVG2zQe strong[data-v-4d6812b6]");
+
+        const goals = await page.evaluate(() => {
+            const goalElement = document.querySelector("div.OlVG2zQe strong[data-v-4d6812b6]");
+            return goalElement.textContent.replace(/[^0-9]/g, "");
         });
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const goalElement = $("div.OlVG2zQe strong[data-v-4d6812b6]").first();
-        const goals = goalElement.text().replace(/[^0-9]/g, "");
 
         const data = {
             player: "Son Heung-min",
@@ -25,11 +24,7 @@ const getGoals = async () => {
         fs.writeFileSync("goals.json", JSON.stringify(data, null, 2));
         console.log("골 수 데이터를 저장했습니다: ", data);
 
-        if (fs.existsSync("goals.json")) {
-            console.log("goals.json 파일이 생성되었습니다.");
-        } else {
-            console.error("goals.json 파일 생성에 실패했습니다.");
-        }
+        await browser.close();
     } catch (error) {
         console.error("에러 발생: ", error);
     }
