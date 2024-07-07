40c6fc306bbb07a2d747c394192368955e15182d
Eun Oh <bdohhhhh@gmail.com>
2024-07-08 07:46:47 +0900

    240708 [fix] : index.js fix 15

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const getGoals = async () => {
         await page.setExtraHTTPHeaders({
             "User-Agent":
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            Accept: "*/*",
         });
 
         // 페이지 이동 및 대기
@@ -19,6 +18,8 @@ const getGoals = async () => {
 
         // 네트워크 활동 대기
         await page.waitForResponse((response) => response.status() === 200);
+
+        console.log(page);
 
         // HTML 콘텐츠 출력
         const content = await page.content();
