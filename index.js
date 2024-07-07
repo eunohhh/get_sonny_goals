578a0756525af67efd6d9fd530f77bf32da1bd49
Eun Oh <bdohhhhh@gmail.com>
2024-07-08 07:33:14 +0900

    240708 [fix] : index.js fix 11

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const fs = require("fs");
+
+// Stealth 플러그인을 사용하도록 설정
+puppeteer.use(StealthPlugin());
 
 const getGoals = async () => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
+
+        // 추가적인 HTTP 헤더 설정
+        await page.setExtraHTTPHeaders({
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            Accept: "*/*",
+        });
+
         // 페이지 이동 및 대기
         await page.goto("https://namu.wiki/w/%EC%86%90%ED%9D%A5%EB%AF%BC", { timeout: 60000 });
 
-        // 네트워크 활동 대기
-        await page.waitForResponse((response) => response.status() === 200);
-
-        // HTML 콘텐츠 출력
-        const content = await page.content();
-        console.log(content);
-
-        // 페이지 스크린샷 찍기 (디버깅용)
-        await page.screenshot({ path: "page.png", fullPage: true });
-
-        // 요소 대기
+        // 페이지가 완전히 로드될 때까지 대기
         const selected = await page.waitForSelector("tbody tr td div.OlVG2zQe strong", { timeout: 60000 });
 
         console.log(selected);
 
-        // 요소 평가
         const goals = await page.evaluate(() => {
             const goalElements = document.querySelectorAll("tbody tr td div.OlVG2zQe strong");
             const goalNumbers = Array.from(goalElements).map((element) => {
