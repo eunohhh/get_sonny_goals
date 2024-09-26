fe40b0ae39047f7967a9c2b4d667a661bad90ff8
Eun Oh <bdohhhhh@gmail.com>
2024-09-26 21:38:35 +0900

    [update]: github action 에러로 인한 소니골 로직 전체 수정

--- index.js
+++ index.js
@@ -1,82 +1,13 @@
-// const { Curl } = require("node-libcurl");
-// const cheerio = require("cheerio");
-// const fs = require("fs");
-
-// const getGoals = async () => {
-//     try {
-//         const curl = new Curl();
-//         curl.setOpt("URL", "https://namu.wiki/w/%EC%86%90%ED%9D%A5%EB%AF%BC");
-//         curl.setOpt("FOLLOWLOCATION", true);
-//         curl.setOpt(
-//             "USERAGENT",
-//             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-//         );
-
-//         curl.on("end", function (statusCode, data, headers) {
-//             console.info(statusCode);
-//             console.info("---");
-//             console.info(data.length);
-//             console.info("---");
-//             console.info(this.getInfo("TOTAL_TIME"));
-
-//             if (statusCode === 200) {
-//                 const $ = cheerio.load(data);
-//                 const goalElements = $(
-//                     "#app > div > div._5DRQ6Phv.a5z6UHjy > article > div.aXXRCC8v > article > div.CVYBZp8c > div > div:nth-child(2) > div.LKVNbZff > div.hh9Bcdpt > div.Bk4No22F.SuHD5-eo > table > tbody > tr:nth-child(16) > td:nth-child(2) > div > strong"
-//                 );
-//                 const goalNumbers = Array.from(goalElements).map((element) => {
-//                     return parseInt(
-//                         $(element)
-//                             .text()
-//                             .replace(/[^0-9]/g, ""),
-//                         10
-//                     );
-//                 });
-//                 const goals = Math.max(...goalNumbers);
-
-//                 const result = {
-//                     player: "Son Heung-min",
-//                     goals: goals,
-//                     timestamp: new Date().toISOString(),
-//                 };
-
-//                 fs.writeFileSync("goals.json", JSON.stringify(result, null, 2));
-//                 console.log("골 수 데이터를 저장했습니다: ", result);
-//             } else {
-//                 console.error(`Failed to load page, status code: ${statusCode}`);
-//             }
-
-//             this.close();
-//         });
-
-//         curl.on("error", function (err) {
-//             console.error(err);
-//             this.close();
-//         });
-
-//         curl.perform();
-//     } catch (error) {
-//         console.error("에러 발생: ", error);
-//         process.exit(1);
-//     }
-// };
-
-// getGoals();
-
-// const puppeteer = require("puppeteer-extra");
-// const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const puppeteer = require("puppeteer");
 const fs = require("fs");
-// const UserAgent = require("user-agents"); // ^1.0.958
 const cheerio = require("cheerio");
 const path = require("path");
 
-// Stealth 플러그인을 사용하도록 설정
-// puppeteer.use(StealthPlugin());
-
 const getGoals = async () => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
         const page = await browser.newPage();
 
         // 추가적인 HTTP 헤더 설정
@@ -85,9 +16,10 @@ const getGoals = async () => {
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         });
 
-        // Set screen size
+        // 화면 크기 설정
         await page.setViewport({ width: 1920, height: 1080 });
 
+        
         // 페이지 이동 및 대기
         await page.goto("https://en.wikipedia.org/wiki/Son_Heung-min", {
             waitUntil: "domcontentloaded",
@@ -96,32 +28,16 @@ const getGoals = async () => {
 
         const content = await page.content();
 
-        // console.log(content);
-        // $에 cheerio를 로드한다.
+        // cheerio 로드
         const $ = cheerio.load(content);
 
+        // "#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(103) > tbody > tr:nth-child(22) > th:nth-child(15)" 한국어 위키일경우
+        // "#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(108) > tbody > tr:nth-child(23) > th:nth-child(11)" 영어 위키일경우
         const goals = $(
-            // "#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(103) > tbody > tr:nth-child(22) > th:nth-child(15)" 한국어 위키일경우
             "#mw-content-text > div.mw-content-ltr.mw-parser-output > table:nth-child(108) > tbody > tr:nth-child(23) > th:nth-child(11)"
         );
 
         console.log("골 수: ", goals.text());
-
-        // // 페이지가 완전히 로드될 때까지 대기
-        // await page.waitForSelector(
-        //     "#app > div > div._5DRQ6Phv.a5z6UHjy > article > div.aXXRCC8v > article > div.CVYBZp8c > div > div:nth-child(2) > div.LKVNbZff > div.hh9Bcdpt > div.Bk4No22F.SuHD5-eo > table > tbody > tr:nth-child(16) > td:nth-child(2) > div > strong",
-        //     { timeout: 60000 }
-        // );
-
-        // const goals = await page.evaluate(() => {
-        //     const goalElements = document.querySelectorAll(
-        //         "#app > div > div._5DRQ6Phv.a5z6UHjy > article > div.aXXRCC8v > article > div.CVYBZp8c > div > div:nth-child(2) > div.LKVNbZff > div.hh9Bcdpt > div.Bk4No22F.SuHD5-eo > table > tbody > tr:nth-child(16) > td:nth-child(2) > div > strong"
-        //     );
-        //     const goalNumbers = Array.from(goalElements).map((element) => {
-        //         return parseInt(element.textContent.replace(/[^0-9]/g, ""), 10);
-        //     });
-        //     return Math.max(...goalNumbers);
-        // });
 
         const data = {
             player: "Son Heung-min",
@@ -143,7 +59,7 @@ const getGoals = async () => {
         await browser.close();
     } catch (error) {
         console.error("에러 발생: ", error);
-        process.exit(1); // 에러 발생 시 스크립트를 종료하고 비정상 종료 상태 코드를 반환합니다.
+        process.exit(1);
     }
 };
 
